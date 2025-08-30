5f2f23bf20de3cea116d501642aa76714f567dad
nitishprajapati5 <nitishprajapati180@gmail.com>
2025-08-31 01:47:31 +0530

    Added Animation

--- assigment_5/src/App.jsx
+++ assigment_5/src/App.jsx
@@ -1,16 +1,18 @@
-import { useState,useEffect, use } from 'react'
-import './App.css'
-import Card from './SubComponent/Card';
-import SearchField from './SubComponent/SearchField';
-import SkeletonLoader from './SubComponent/Loader/SkeletonLoader';
-import Error from './SubComponent/Error';
+import { useState, useEffect, use } from "react";
+import "./App.css";
+import Card from "./SubComponent/Card";
+import SearchField from "./SubComponent/SearchField";
+import SkeletonLoader from "./SubComponent/Loader/SkeletonLoader";
+import Error from "./SubComponent/Error";
+import { ArrowUpNarrowWide,ArrowDownWideNarrow } from "lucide-react";
+import { motion } from "framer-motion";
 
 function App() {
-  
   const [isLoading, setIsLoading] = useState(true);
   const [games, setGames] = useState([]);
-  const [search,setSearchItem] = useState("")
+  const [search, setSearchItem] = useState("");
   const [error, setError] = useState(null);
+  const [platformFilter, setPlatformFilter] = useState(false);
 
   useEffect(() => {
     setIsLoading(true);
@@ -23,8 +25,8 @@ function App() {
         const data = await response.json();
 
         console.log("Fetched data:", data);
-        setGames(data.slice(1)); 
-        setIsLoading(false)
+        setGames(data.slice(1));
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
         setError("Could not load game data. Please try again later.");
@@ -34,51 +36,70 @@ function App() {
     };
     fetchData();
   }, []);
-  
-  if(isLoading){
-  return <SkeletonLoader />
+
+  const handlePlatformFilter = () => {
+    setPlatformFilter(!platformFilter);
+  };
+
+  if (isLoading) {
+    return <SkeletonLoader />;
   }
 
   if (error) {
-    return (
-      <Error error={error}/>
-    );
+    return <Error error={error} />;
   }
 
   return (
     <>
-      {
-        !isLoading && (
-          <>
-            <div className="stars"></div>
-      <div className="shooting-star"></div>
-      <div className="shooting-star"></div>
-      <div className="shooting-star"></div>
-      <div className="shooting-star"></div>
-      <div className="shooting-star"></div>
+      {!isLoading && (
+        <>
+          <div className="stars"></div>
+          <div className="shooting-star"></div>
+          <div className="shooting-star"></div>
+          <div className="shooting-star"></div>
+          <div className="shooting-star"></div>
+          <div className="shooting-star"></div>
 
-      <div className="flex flex-col justify-center mt-12">
-        <div className="px-10 py-4 rounded-2xl backdrop-blur-lg bg-white/30 border border-white/40 mt-2">
-          <h1 className="text-6xl font-extrabold text-transparent bg-clip-text 
+          <div className="flex flex-col justify-center mt-12">
+            <div className="px-10 py-4 rounded-2xl backdrop-blur-lg bg-white/30 border border-white/40 mt-2">
+              <h1
+                className="text-6xl font-extrabold text-transparent bg-clip-text 
             bg-gradient-to-r from-amber-500 via-pink-500 to-purple-600 
-            drop-shadow-lg text-center">
-            Games Arena
-          </h1>
-        </div>
-        <div className='w-full px-10 py-4 rounded-2xl backdrop-blur-lg bg-white/30 border border-white/40 mt-2 flex justify-center'>
-          <SearchField setSearchItem={setSearchItem}/>
-        </div>
-      </div>
-      <div className='container mx-auto'>
-        <div className='grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-6'>
-          <Card data={games} searchKeyWord={search}/>
-        </div>
-      </div>
-          </>
-        )
-      }
+            drop-shadow-lg text-center"
+              >
+                Games Arena
+              </h1>
+            </div>
+            <div className="w-full px-10 py-4 rounded-2xl backdrop-blur-lg bg-white/30 border border-white/40 mt-2 flex flex-row justify-between">
+              <SearchField setSearchItem={setSearchItem} />
+            </div>
+            <div className="flex flex-row w-full justify-center mt-2 items-center gap-6 space-x-3">
+              <motion.button
+                onClick={() => setPlatformFilter(!platformFilter)}
+                type="submit"
+                whileTap={{ scale: 0.9 ,rotate: 2}}
+                whileHover={{scale:1.05}}
+                transition={{type:"spring",stiffness:300,damping:15}}
+                className=" cursor-pointer flex flex-row items-center justify-center bg-blue-300 p-4 rounded-2xl font-bold"
+              >
+                Filter Based on Platform
+                {!platformFilter ? <ArrowUpNarrowWide className="gap-6" /> : < ArrowDownWideNarrow/>}
+              </motion.button>
+            </div>
+          </div>
+          <div className="container mx-auto">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 p-6">
+              <Card
+                data={games}
+                searchKeyWord={search}
+                isPlatformFilterApplied={platformFilter}
+              />
+            </div>
+          </div>
+        </>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- assigment_5/src/SubComponent/Card.jsx
+++ assigment_5/src/SubComponent/Card.jsx
@@ -1,51 +1,86 @@
-import React from 'react'
-import { Award,Trophy,Computer,Gamepad2 } from 'lucide-react';
+import React from "react";
+import { Award, Trophy, Computer, Gamepad2 } from "lucide-react";
+import {motion} from "framer-motion"
 
 export default function Card(props) {
+  console.log("Card props:", props);
+  let filteredData = props.data;
+  if (props.isPlatformFilterApplied === true) {
+    filteredData = [...filteredData].sort((a, b) =>
+      a.platform.localeCompare(b.platform)
+    );
+  }
+  if (
+    props.searchKeyWord.length > 0
+  ) {
     
-   const data = props.searchKeyWord.length > 0 ? props.data.filter(game => 
-        game.title.toLowerCase().includes(props.searchKeyWord.toLowerCase()) ||
-        game.genre.toLowerCase().includes(props.searchKeyWord.toLowerCase()) ||
-        game.platform.toLowerCase().includes(props.searchKeyWord.toLowerCase()
-    )) : props.data; 
-
+    filteredData = props.data.filter((game) =>
+      game.title.toLowerCase().includes(props.searchKeyWord.toLowerCase())
+    );
+    if(props.isPlatformFilterApplied === true){
+      filteredData = [...filteredData].sort((a, b) =>
+        a.platform.localeCompare(b.platform)
+      );
+    }
+  }
+  
   return (
     <>
-     {data.map((game, index) => (
-        <div 
-          key={index} 
+      {filteredData.map((game, index) => (
+        <motion.div
+        initial={{opacity:0,y:40}}
+        animate={{opacity:1,y:0}}
+        transition={{duration:0.4,delay:index*0.05}}
+        whileHover={{scale:1.05,rotate:1}}
+        whileTap={{scale:0.5,rotate:-2}}
+          key={index}
           className="relative backdrop-blur-lg bg-white/30 border border-white/40 
                      rounded-2xl shadow-lg p-6 flex flex-col justify-between 
                      transform transition duration-300 hover:scale-105 hover:shadow-2xl"
         >
           {/* Award Badge (floating top-right) */}
-          {game.editors_choice === 'Y' && (
-            <div className="absolute top-0 right-0 translate-x-3 flex items-center space-x-1 
+          {game.editors_choice === "Y" && (
+            <div
+              className="absolute top-0 right-5 translate-x-3 flex items-center space-x-1 
                             bg-gradient-to-r from-purple-600 to-pink-500 
-                            text-white px-2 py-1 rounded-full shadow-md">
+                            text-white px-2 py-1 rounded-full shadow-md z-50"
+            >
               <Award size={18} className="text-yellow-300 scale-110" />
-              {/* <span className="text-xs font-semibold">Editor’s Choice</span> */}
+              <span className="text-xs font-semibold z-50">Editor’s Choice</span>
             </div>
           )}
 
           {/* Title */}
-          <h2 className="text-black text-xl font-bold bg-clip-text text-ellipsis min-w-0.5 
-                          ">
+          <h2
+            className="text-black text-xl font-bold bg-clip-text text-ellipsis min-w-0.5 
+                          "
+          >
             {game.title}
           </h2>
 
           {/* Info */}
-          <p className="flex flex-row gap-2 text-sm text-gray-800 m-2 font-bold"><Gamepad2 /> Genre: {game.genre}</p>
-          <p className="flex flex-row gap-2 text-sm text-gray-800 m-2 font-bold"><Computer /> Platform: <span className=''>{game.platform}</span></p>
+          <p className="flex flex-row gap-2 text-sm text-gray-800 m-2 font-bold">
+            <Gamepad2 /> Genre: {game.genre}
+          </p>
+          <p className="flex flex-row gap-2 text-sm text-gray-800 m-2 font-bold">
+            <Computer /> Platform: <span className="">{game.platform}</span>
+          </p>
           <p className="flex flex-row gap-2 text-sm font-semibold m-2">
-            <Trophy className='' style={{color:'green'}} size={18}/> Score: 
-            <span className={`ml-1 ${game.score >= 8 ? 'text-green-600' : game.score >= 6 ? 'text-yellow-600' : 'text-red-600'}`}>
+            <Trophy className="" style={{ color: "green" }} size={18} /> Score:
+            <span
+              className={`ml-1 ${
+                game.score >= 8
+                  ? "text-green-600"
+                  : game.score >= 6
+                  ? "text-yellow-600"
+                  : "text-red-600"
+              }`}
+            >
               {game.score}/10
             </span>
           </p>
-        </div>
+        </motion.div>
       ))}
     </>
-
-  )
+  );
 }
